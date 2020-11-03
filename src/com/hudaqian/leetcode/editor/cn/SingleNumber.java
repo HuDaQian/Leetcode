a902e48d18b032bc509e24206409bda40a4cef49
hudaqian <458331943@qq.com>
2020-11-03 09:30:15 +0800

    2020-11-03
    136. 只出现一次的数字

---
+++ src/com/hudaqian/leetcode/editor/cn/SingleNumber.java
@@ -0,0 +1,58 @@
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,1]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: [4,1,2,1,2]
+//输出: 4 
+// Related Topics 位运算 哈希表 
+// 👍 1565 👎 0
+
+
+package com.hudaqian.leetcode.editor.cn;
+
+import java.util.Arrays;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        Solution solution = new SingleNumber().new Solution();
+        int[] nums = {1, 2, 3, 4, 1, 2, 3};
+        System.out.print(solution.singleNumber(nums));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int singleNumber(int[] nums) {
+            /**
+             * 解法1：排序 待修改
+             */
+//            Arrays.sort(nums);
+//            for (int i = 0; i < nums.length; i++) {
+//                if (i == nums.length - 1 || nums[i] != nums[i + 1]) {
+//                    return nums[i];
+//                }
+//                i++;
+//            }
+//            return -1;
+            /**
+             * 解法2：异或
+             */
+            int ans = 0;
+            for (int num : nums) {
+                ans = ans ^ num;
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/com/hudaqian/leetcode/editor/cn/SingleNumberIii.java
@@ -0,0 +1,47 @@
+//给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。 
+//
+// 示例 : 
+//
+// 输入: [1,2,1,3,2,5]
+//输出: [3,5] 
+//
+// 注意： 
+//
+// 
+// 结果输出的顺序并不重要，对于上面的例子， [5, 3] 也是正确答案。 
+// 你的算法应该具有线性时间复杂度。你能否仅使用常数空间复杂度来实现？ 
+// 
+// Related Topics 位运算 
+// 👍 313 👎 0
+
+
+    package com.hudaqian.leetcode.editor.cn;
+
+import java.util.Arrays;
+
+public class SingleNumberIii {
+        public static void main(String[] args) {
+            Solution solution = new SingleNumberIii().new Solution();
+            int[] nums = {1,1,2,2,3,3,4};
+            System.out.print(solution.singleNumber(nums)[0]);
+        }
+       //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int[] res = new int[nums.length];
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length;i++) {
+            if (nums[i] != nums[i-1]) {
+                res[count] = nums[i-1];
+                count++;
+            } else {
+                i++;
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    }
