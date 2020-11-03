ebf4cf62eb685f3a867057830d8064e8c8606630
hudaqian <458331943@qq.com>
2020-11-04 07:55:07 +0800

    349. 两个数组的交集
    哈希集合解法

--- src/com/hudaqian/leetcode/editor/cn/IntersectionOfTwoArrays.java
+++ src/com/hudaqian/leetcode/editor/cn/IntersectionOfTwoArrays.java
@@ -27,7 +27,7 @@
 
 package com.hudaqian.leetcode.editor.cn;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class IntersectionOfTwoArrays {
     public static void main(String[] args) {
@@ -43,26 +43,53 @@ public class IntersectionOfTwoArrays {
             /**
              * 解法1：排序
              */
-            Arrays.sort(nums1);
-            Arrays.sort(nums2);
-            int i = 0, j = 0, count = 0;
-            int[] res = new int[Math.min(nums1.length, nums2.length)];
-            while (i < nums1.length && j < nums2.length) {
-                if (nums1[i] == nums2[j]) {
-                    if (count == 0 || res[count - 1] != nums1[i]) {
-                        res[count] = nums1[i];
-                        count++;
-                    }
-                    i++;
-                    j++;
-                } else if (nums1[i] < nums2[j]) {
-                    i++;
-                } else {
-                    j++;
+//            Arrays.sort(nums1);
+//            Arrays.sort(nums2);
+//            int i = 0, j = 0, count = 0;
+//            int[] res = new int[Math.min(nums1.length, nums2.length)];
+//            while (i < nums1.length && j < nums2.length) {
+//                if (nums1[i] == nums2[j]) {
+//                    if (count == 0 || res[count - 1] != nums1[i]) {
+//                        res[count] = nums1[i];
+//                        count++;
+//                    }
+//                    i++;
+//                    j++;
+//                } else if (nums1[i] < nums2[j]) {
+//                    i++;
+//                } else {
+//                    j++;
+//                }
+//            }
+//            return Arrays.copyOfRange(res, 0, count);
+
+            /**
+             * 解法2：哈希集合解法
+             */
+            Set<Integer> set1 = new HashSet<>();
+            Set<Integer> set2 = new HashSet<>();
+            for (int num : nums1) {
+                set1.add(num);
+            }
+            for (int num : nums2) {
+                set2.add(num);
+            }
+            return getIntersection(set1, set2);
+        }
+
+        private int[] getIntersection(Set<Integer> set1, Set<Integer> set2) {
+            if (set1.size() > set2.size()) {
+                return getIntersection(set2, set1);
+            }
+            int[] res = new int[set1.size()];
+            int count = 0;
+            for (Integer num : set1) {
+                if (set2.contains(num)) {
+                    res[count] = num;
+                    count++;
                 }
             }
             return Arrays.copyOfRange(res, 0, count);
-
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
