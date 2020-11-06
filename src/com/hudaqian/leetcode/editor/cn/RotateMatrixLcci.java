1dc65f2851e07b1f7e10ceffd69f3983884101df
hudaqian <458331943@qq.com>
2020-11-06 22:21:54 +0800

    48. 旋转图像
    外圈到内圈 四格旋转

--- src/com/hudaqian/leetcode/editor/cn/RotateMatrixLcci.java
+++ src/com/hudaqian/leetcode/editor/cn/RotateMatrixLcci.java
@@ -48,7 +48,7 @@ package com.hudaqian.leetcode.editor.cn;
 public class RotateMatrixLcci {
     public static void main(String[] args) {
         Solution solution = new RotateMatrixLcci().new Solution();
-        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix = {{5,1,9,11},{2,4,8,10}, {13,3,6,7}, {15,14,12,16}};
         solution.rotate(matrix);
         System.out.print(matrix);
     }
@@ -58,25 +58,79 @@ public class RotateMatrixLcci {
         public void rotate(int[][] matrix) {
             int length = matrix.length;
             /**
-             * x y轴替换
+             * 解法1：转轴+反转
              */
-            for (int i = 0; i < length; i++) {
-                for (int j = i; j < length; j++) {
+//            /**
+//             * x y轴替换
+//             */
+//            for (int i = 0; i < length; i++) {
+//                for (int j = i; j < length; j++) {
+//                    int temp = matrix[i][j];
+//                    matrix[i][j] = matrix[j][i];
+//                    matrix[j][i] = temp;
+//                }
+//            }
+//            /**
+//             * 横向反转
+//             */
+//            for (int i = 0; i < length; i++) {
+//                for (int j = 0; j < length/2; j++) {
+//                    int temp = matrix[i][j];
+//                    matrix[i][j] = matrix[i][length - j - 1];
+//                    matrix[i][length - j - 1] = temp;
+//                }
+//            }
+            /**
+             * 解法2：外圈到内圈 四格旋转
+             *
+             * i表示层数
+             * j表示每层旋转的次数 从第i格子开始到距离边界i个格子结束
+             *
+             * 0,0
+             * 0,length-1
+             * length-1,length-1
+             * length-1,0
+             *
+             * 0,j
+             * j,length-1
+             * length-1,length-1-j
+             * length-1-j,0
+             *
+             * i,j
+             * j,length-1-i
+             * length-1-i,length-1-j
+             * length-1-j,i
+             */
+            for (int i = 0; i < (length+1)/2; i++) {
+                for (int j = i; j < (length-i-1); j++) {
                     int temp = matrix[i][j];
-                    matrix[i][j] = matrix[j][i];
-                    matrix[j][i] = temp;
+                    matrix[i][j] = matrix[length-1-j][i];
+                    matrix[length-1-j][i] = matrix[length-i-1][length-j-1];
+                    matrix[length-i-1][length-j-1] = matrix[j][length-1-i];
+                    matrix[j][length-1-i] = temp;
                 }
             }
             /**
-             * 横向反转
+             * 5,1,9,11
+             * 2,4,8,10
+             * 13,3,6,7
+             * 15,14,12,16
+             *
+             * 15,1,9,5
+             * 2,4,8,10
+             * 13,3,6,7
+             * 16,14,12,11
+             *
+             * 15,13,2,5
+             * 14,6,3,1
+             * 12,8,4,9
+             * 16,7,10,11
+             *
+             * 15,13,9,5
+             * 14,3,4,1
+             * 12,6,8,9
+             * 16,7,10,11
              */
-            for (int i = 0; i < length; i++) {
-                for (int j = 0; j < length/2; j++) {
-                    int temp = matrix[i][j];
-                    matrix[i][j] = matrix[i][length - j - 1];
-                    matrix[i][length - j - 1] = temp;
-                }
-            }
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
