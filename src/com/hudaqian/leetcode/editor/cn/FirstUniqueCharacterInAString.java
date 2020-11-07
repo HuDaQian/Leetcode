a0129b9644d4cc8608050e3f6b378188acf58d22
hudaqian <458331943@qq.com>
2020-11-07 08:51:35 +0800

    387. 字符串中的第一个唯一字符
    数组写法

--- src/com/hudaqian/leetcode/editor/cn/FirstUniqueCharacterInAString.java
+++ src/com/hudaqian/leetcode/editor/cn/FirstUniqueCharacterInAString.java
@@ -52,6 +52,18 @@ class Solution {
         /**
          * 数组写法
          */
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            count[c-'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (count[c-'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
