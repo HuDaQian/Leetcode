548c3b82d79bbaeb3ae8ac51b1e22a22703b947b
hudaqian <458331943@qq.com>
2020-11-07 09:16:43 +0800

    242.有效的字母异位词
    哈希算法

--- src/com/hudaqian/leetcode/editor/cn/FirstUniqueCharacterInAString.java
+++ src/com/hudaqian/leetcode/editor/cn/FirstUniqueCharacterInAString.java
@@ -54,11 +54,11 @@ class Solution {
          */
         int[] count = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
+            char c = s.charAt(i);
             count[c-'a']++;
         }
         for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
+            char c = s.charAt(i);
             if (count[c-'a'] == 1) {
                 return i;
             }
--- src/com/hudaqian/leetcode/editor/cn/ValidAnagram.java
+++ src/com/hudaqian/leetcode/editor/cn/ValidAnagram.java
@@ -20,36 +20,70 @@
 // 👍 272 👎 0
 
 
-    package com.hudaqian.leetcode.editor.cn;
+package com.hudaqian.leetcode.editor.cn;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ValidAnagram {
-        public static void main(String[] args) {
-            Solution solution = new ValidAnagram().new Solution();
-            String s = "anagram";
-            String t = "nagaram";
-            System.out.print(solution.isAnagram(s,t));
-        }
-       //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        /**
-         * 两个字符串相等也算字母异位词
-         */
+    public static void main(String[] args) {
+        Solution solution = new ValidAnagram().new Solution();
+        String s = "aacc";
+        String t = "ccac";
+        System.out.print(solution.isAnagram(s, t));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            /**
+             * 两个字符串相等也算字母异位词
+             */
 //        if (s.equals(t)) {
 //            return false;
 //        }
-        char[] schar = s.toCharArray();
-        char[] tchar = t.toCharArray();
-        Arrays.sort(schar);
-        Arrays.sort(tchar);
-        return Arrays.equals(schar, tchar);
+//        char[] schar = s.toCharArray();
+//        char[] tchar = t.toCharArray();
+//        Arrays.sort(schar);
+//        Arrays.sort(tchar);
+//        return Arrays.equals(schar, tchar);
+            /**
+             * hashmap 有点慢
+             */
+//            HashMap<Character, Integer> map = new HashMap<>();
+//            for (int i = 0; i < s.length(); i++) {
+//                Character c = s.charAt(i);
+//                map.put(c, map.getOrDefault(c, 0) + 1);
+//            }
+//            for (int i = 0; i < t.length(); i++) {
+//                Character c = t.charAt(i);
+//                map.put(c,map.getOrDefault(c,0)-1);
+//                if (map.get(c) < 0) {
+//                    return false;
+//                }
+//            }
+//            return true;
+            /**
+             * 数组统计 如果有unicode字符的话 则需要使用大的hash表
+             */
+            int[] num = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                char schar = s.charAt(i);
+                num[schar - 'a']++;
+            }
+            for (int i = 0; i < t.length(); i++) {
+                char tchar = t.charAt(i);
+                num[tchar - 'a']--;
+                if (num[tchar-'a'] < 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
-    }
+}
