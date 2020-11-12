b269138fa824b9d3e592639e0e0c484b0960f6d9
hudaqian <458331943@qq.com>
2020-11-12 15:47:04 +0800

    104.二叉树的最大深度
    广度优先搜索

--- src/com/hudaqian/leetcode/editor/cn/MaximumDepthOfBinaryTree.java
+++ src/com/hudaqian/leetcode/editor/cn/MaximumDepthOfBinaryTree.java
@@ -92,11 +92,31 @@ public class MaximumDepthOfBinaryTree {
              */
             if (root == null) {
                 return 0;
-            } else {
-                int leftDeepth = maxDepth(root.left);
-                int rightDeepth = maxDepth(root.right);
-                return Math.max(leftDeepth, rightDeepth) + 1;
             }
+            int leftDeepth = maxDepth(root.left);
+            int rightDeepth = maxDepth(root.right);
+            return Math.max(leftDeepth, rightDeepth) + 1;
+            /**
+             * 解法2：广度优先搜索
+             */
+//            Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//            queue.offer(root);
+//            int ans = 0;
+//            while (!queue.isEmpty()) {
+//                int size = queue.size();
+//                while (size > 0) {
+//                    TreeNode node = queue.poll();
+//                    if (node.left != null) {
+//                        queue.offer(node.left);
+//                    }
+//                    if (node.right != null) {
+//                        queue.offer(node.right);
+//                    }
+//                    size--;
+//                }
+//                ans++;
+//            }
+//            return ans;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
