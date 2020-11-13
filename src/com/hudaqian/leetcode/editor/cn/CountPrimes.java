defe14755b1cb1464df615361563943a95fffb50
hudaqian <458331943@qq.com>
2020-11-13 14:56:16 +0800

    204.计数质数
    厄拉多塞筛法
    质数的倍数不是质数

--- src/com/hudaqian/leetcode/editor/cn/CountPrimes.java
+++ src/com/hudaqian/leetcode/editor/cn/CountPrimes.java
@@ -34,7 +34,9 @@
 
 package com.hudaqian.leetcode.editor.cn;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CountPrimes {
@@ -47,38 +49,58 @@ public class CountPrimes {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int countPrimes(int n) {
+//            /**
+//             * 暴力破解法
+//             */
+//            if (n < 3) {
+//                return 0;
+//            }
+//
+//
+//            Set<Integer> set = new HashSet<Integer>();
+//            int count = 1;
+//            for (int i = 3; i < n; i++) {
+//                /**
+//                 * 偶数不为质数
+//                 */
+//                if (i % 2 == 0) {
+//                    continue;
+//                }
+//                boolean sign = true;
+//                //用 j * j <= i 代替 j <= √i 会更好。
+//                //因为我们已经排除了所有偶数，所以每次循环加二将规避偶数会减少循环次数
+//                for (int j = 3; j * j <= i; j += 2) {
+//                    if (i % j == 0) {
+//                        sign = false;
+//                        break;
+//                    }
+//                }
+//                if (sign)
+//                    count++;
+//            }
+//            return count;
             /**
-             * 暴力破解法
+             * 厄拉多塞筛法
+             * 质数的倍数不是质数
              */
             if (n < 3) {
                 return 0;
             }
-
-
-            Set<Integer> set = new HashSet<Integer>();
+            boolean[] isNotPrimes = new boolean[n];
+            //  0 1
+            isNotPrimes[0] = true;
+            isNotPrimes[1] = true;
             int count = 1;
-            for (int i = 3; i < n; i++) {
-                /**
-                 * 偶数不为质数
-                 */
-                if (i % 2 == 0) {
-                    continue;
-                }
-                boolean sign = true;
-                //用 j * j <= i 代替 j <= √i 会更好。
-                //因为我们已经排除了所有偶数，所以每次循环加二将规避偶数会减少循环次数
-                for (int j = 3; j * j <= i; j += 2) {
-                    if (i % j == 0) {
-                        sign = false;
-                        break;
+            for (int i = 3; i < n; i+=2) {
+                if (!isNotPrimes[i]) {
+                    count++;
+                    for(int j = 3; i * j < n; j += 2){
+                        isNotPrimes[i * j] = true;    // 将当前质数的奇数倍都设置成非质数标记 true
                     }
                 }
-                if (sign)
-                    count++;
             }
             return count;
         }
-
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
