9674d060cded7407cae5828603b39c62c6d88696
hudaqian <458331943@qq.com>
2020-11-16 15:44:05 +0800

    137.只出现一次的数字2
    hashSet解法

---
+++ src/com/hudaqian/leetcode/editor/cn/SingleNumberIi.java
@@ -0,0 +1,52 @@
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。 
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,3,2]
+//输出: 3
+// 
+//
+// 示例 2: 
+//
+// 输入: [0,1,0,1,0,1,99]
+//输出: 99 
+// Related Topics 位运算 
+// 👍 459 👎 0
+
+
+package com.hudaqian.leetcode.editor.cn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingleNumberIi {
+    public static void main(String[] args) {
+        Solution solution = new SingleNumberIi;
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int singleNumber(int[] nums) {
+            /**
+             * hashset
+             */
+            Set<Long> set = new HashSet<>();
+            int sumall = 0;
+            for (int num : nums) {
+                set.add((long)num);
+                sumall += num;
+            }
+            int sum = 0;
+            for (long num:set) {
+                sum += num;
+            }
+            return (int)(sum*3 - sumall)/2;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
