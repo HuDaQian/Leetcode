4e1a15283c6ee0f1940891449eff1e08cd275277
hudaqian <458331943@qq.com>
2020-11-16 10:11:50 +0800

    面试题2.6 回文链表
    递归解法

---
+++ src/com/hudaqian/leetcode/editor/cn/PalindromeLinkedListLcci.java
@@ -0,0 +1,97 @@
+//编写一个函数，检查输入的链表是否是回文的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入： 1->2
+//输出： false 
+// 
+//
+// 示例 2： 
+//
+// 输入： 1->2->2->1
+//输出： true 
+// 
+//
+// 
+//
+// 进阶： 
+//你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？ 
+// Related Topics 链表 
+// 👍 35 👎 0
+
+
+package com.hudaqian.leetcode.editor.cn;
+
+public class PalindromeLinkedListLcci {
+    public static void main(String[] args) {
+        Solution solution = new PalindromeLinkedListLcci().new Solution();
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public String print(ListNode listNode) {
+            StringBuilder sb = new StringBuilder();
+            while (listNode != null) {
+                sb.append(listNode.val);
+                listNode = listNode.next;
+            }
+            return sb.toString();
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isPalindrome(ListNode head) {
+            /**
+             * 递归解法
+             * 递归解法是从首排到尾 然后跟从尾排到首 比较
+             */
+            if (head == null) {
+                return true;
+            }
+            fontNode = head;
+            return recursivelyCheck(head);
+        }
+
+        ListNode fontNode = null;
+
+        private boolean recursivelyCheck(ListNode currentNode) {
+            if (currentNode != null) {
+                if (!recursivelyCheck(currentNode.next)) {
+                    return false;
+                }
+                if (currentNode.val != fontNode.val) {
+                    return false;
+                }
+                fontNode = fontNode.next;
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
