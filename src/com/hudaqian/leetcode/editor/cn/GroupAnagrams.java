b6578aff72d060d633bda0d38bc5c44cbed49652
hudaqian <458331943@qq.com>
2020-11-23 16:45:18 +0800

    49.字母异位词分组
    字符排序比较 做key解法

--- src/com/hudaqian/leetcode/editor/cn/GroupAnagrams.java
+++ src/com/hudaqian/leetcode/editor/cn/GroupAnagrams.java
@@ -35,39 +35,39 @@ public class GroupAnagrams {
     class Solution {
         public List<List<String>> groupAnagrams(String[] strs) {
             //  排序 然后维护一个 String ： List 的Map就可以了
-//            Map<String, List<String>> map = new HashMap<>();
-//            for (String str:strs) {
-//                char[] chars = str.toCharArray();
-//                Arrays.sort(chars);
-//                String key = String.copyValueOf(chars);
-//                if (!map.containsKey(key)) {
-//                    map.put(key, new ArrayList<>());
-//                }
-//                map.get(key).add(str);
-//            }
-//            return new ArrayList<>(map.values());
-            /**
-             * 解法2 用一个字符+数字的字符串 a1b2... 来表示key
-             */
             Map<String, List<String>> map = new HashMap<>();
-            int[] count = new int[26];
-            for (String s : strs) {
-                Arrays.fill(count, 0);
-                for (char c : s.toCharArray()) count[c - 'a']++;
-
-                StringBuilder sb = new StringBuilder("");
-                for (int i = 0; i < 26; i++) {
-                    sb.append('#');
-                    sb.append(count[i]);
-                }
-                String key = sb.toString();
-
+            for (String str:strs) {
+                char[] chars = str.toCharArray();
+                Arrays.sort(chars);
+                String key = String.copyValueOf(chars);
                 if (!map.containsKey(key)) {
                     map.put(key, new ArrayList<>());
                 }
-                map.get(key).add(s);
+                map.get(key).add(str);
             }
             return new ArrayList<>(map.values());
+            /**
+             * 解法2 用一个字符+数字的字符串 a1b2... 来表示key
+             */
+//            Map<String, List<String>> map = new HashMap<>();
+//            int[] count = new int[26];
+//            for (String s : strs) {
+//                Arrays.fill(count, 0);
+//                for (char c : s.toCharArray()) count[c - 'a']++;
+//
+//                StringBuilder sb = new StringBuilder("");
+//                for (int i = 0; i < 26; i++) {
+//                    sb.append('#');
+//                    sb.append(count[i]);
+//                }
+//                String key = sb.toString();
+//
+//                if (!map.containsKey(key)) {
+//                    map.put(key, new ArrayList<>());
+//                }
+//                map.get(key).add(s);
+//            }
+//            return new ArrayList<>(map.values());
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
