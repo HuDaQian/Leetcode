bde91a9013a79240947be3b0c52b144742f71ae9
hudaqian <458331943@qq.com>
2020-11-24 12:01:22 +0800

    220.存在重复元素 III
    二叉搜索树

--- src/com/hudaqian/leetcode/editor/cn/ContainsDuplicateIii.java
+++ src/com/hudaqian/leetcode/editor/cn/ContainsDuplicateIii.java
@@ -28,6 +28,7 @@ package com.hudaqian.leetcode.editor.cn;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class ContainsDuplicateIii {
     public static void main(String[] args) {
@@ -67,6 +68,25 @@ public class ContainsDuplicateIii {
 //
 //            }
 //            return false;
+            /**
+             * 二叉搜索树
+             */
+            TreeSet<Integer> set = new TreeSet<>();
+            for (int i = 0; i < nums.length; ++i) {
+                // Find the successor of current element
+                Integer s = set.ceiling(nums[i]);
+                if (s != null && (s <= nums[i] + t || s <= Integer.MIN_VALUE + t)) return true;
+
+                // Find the predecessor of current element
+                Integer g = set.floor(nums[i]);
+                if (g != null && (nums[i] <= g + t || g >= Integer.MAX_VALUE - t)) return true;
+
+                set.add(nums[i]);
+                if (set.size() > k) {
+                    set.remove(nums[i - k]);
+                }
+            }
+            return false;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
