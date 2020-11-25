5a3456a2effe03aed4149c1b92fe8d19b3e71f30
hudaqian <458331943@qq.com>
2020-11-25 09:46:25 +0800

    162.寻找峰值
    迭代法+二分查找

--- src/com/hudaqian/leetcode/editor/cn/GuessNumberHigherOrLower.java
+++ src/com/hudaqian/leetcode/editor/cn/GuessNumberHigherOrLower.java
@@ -65,6 +65,15 @@ public class GuessNumberHigherOrLower {
         System.out.print(solution.guessNumber(10));
     }
 
+    /**
+     * Forward declaration of guess API.
+     *
+     * param num your guess
+     * return -1 if num is lower than the guess number
+     * 1 if num is higher than the guess number
+     * otherwise return 0
+     * int guess(int num);
+     */
     public static class GuessGame {
         static int pick;
 
@@ -82,15 +91,6 @@ public class GuessNumberHigherOrLower {
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
-    /**
-     * Forward declaration of guess API.
-     *
-     * @param num your guess
-     * @return -1 if num is lower than the guess number
-     * 1 if num is higher than the guess number
-     * otherwise return 0
-     * int guess(int num);
-     */
 
     public class Solution extends GuessGame {
         public int guessNumber(int n) {
