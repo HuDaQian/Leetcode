79f5ef1ae3a58c19b6d630968d7ccd7242b73c3f
hudaqian <458331943@qq.com>
2020-12-01 10:22:06 +0800

    448.找到所有数组中消失的数字
    使用额外空间的做法1 哈希表

---
+++ src/com/hudaqian/leetcode/editor/cn/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,52 @@
+//给定一个范围在 1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。 
+//
+// 找到所有在 [1, n] 范围之间没有出现在数组中的数字。 
+//
+// 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。 
+//
+// 示例: 
+//
+// 
+//输入:
+//[4,3,2,7,8,2,3,1]
+//
+//输出:
+//[5,6]
+// 
+// Related Topics 数组 
+// 👍 505 👎 0
+
+
+package com.hudaqian.leetcode.editor.cn;
+
+import java.util.*;
+
+public class FindAllNumbersDisappearedInAnArray {
+    public static void main(String[] args) {
+        Solution solution = new FindAllNumbersDisappearedInAnArray().new Solution();
+        int[] nums = {1,2,3,4,4,5,7,7};
+        System.out.print(solution.findDisappearedNumbers(nums).toString());
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            /**
+             * 解法1：使用额外空间的做法1 哈希表
+             */
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int num:nums) {
+                map.put(num,map.getOrDefault(num,0)+1);
+            }
+            List<Integer> result = new ArrayList<>();
+            for (int i = 1; i <= nums.length; i++) {
+                if (!map.containsKey(i)) {
+                    result.add(i);
+                }
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
