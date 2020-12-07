cc8f758938d83286c4450d0ea457f0809d2a621f
hudaqian <458331943@qq.com>
2020-12-07 10:19:29 +0800

    101.对称二叉树
    递归写法

--- src/com/hudaqian/leetcode/editor/cn/SymmetricTree.java
+++ src/com/hudaqian/leetcode/editor/cn/SymmetricTree.java
@@ -1,56 +1,85 @@
-//给定一个二叉树，检查它是否是镜像对称的。 
+//给定一个二叉树，检查它是否是镜像对称的。
 //
-// 
 //
-// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
 //
 //     1
 //   / \
 //  2   2
 // / \ / \
 //3  4 4  3
-// 
 //
-// 
 //
-// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
 //
 //     1
 //   / \
 //  2   2
 //   \   \
 //   3    3
-// 
 //
-// 
 //
-// 进阶： 
 //
-// 你可以运用递归和迭代两种方法解决这个问题吗？ 
-// Related Topics 树 深度优先搜索 广度优先搜索 
+//
+// 进阶：
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？
+// Related Topics 树 深度优先搜索 广度优先搜索
 // 👍 1148 👎 0
 
 
-    package com.hudaqian.leetcode.editor.cn;
-    public class SymmetricTree {
-        public static void main(String[] args) {
-            Solution solution = new SymmetricTree;
-        }
-       //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
+package com.hudaqian.leetcode.editor.cn;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+        Solution solution = new SymmetricTree().new Solution();
         
     }
-}
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            return isCheck(root, root);
+        }
+
+        private boolean isCheck(TreeNode leftNode, TreeNode rightNode) {
+            if (leftNode == null && rightNode == null) return true;
+            if (leftNode == null || rightNode == null) return false;
+            return leftNode.val == rightNode.val && isCheck(leftNode.left, rightNode.right) && isCheck(leftNode.right, rightNode.left);
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-    }
+}
