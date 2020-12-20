d43477ca4fff9b3af8ee658907cacf84ec0225a1
hudaqian <458331943@qq.com>
2020-12-20 15:55:30 +0800

    5630.删除子数组的最大得分
    滑动窗口 + 数组解法

--- src/com/hudaqian/leetcode/editor/cn/MaximumErasureValue.java
+++ src/com/hudaqian/leetcode/editor/cn/MaximumErasureValue.java
@@ -44,7 +44,8 @@ import java.util.Set;
 public class MaximumErasureValue {
     public static void main(String[] args) {
         Solution solution = new MaximumErasureValue().new Solution();
-        int[] nums = {4,2,4,5,6,7};
+        int[] nums = {187, 470, 25, 436, 538, 809, 441, 167, 477, 110, 275, 133, 666, 345, 411, 459, 490, 266, 987, 965, 429, 166, 809, 340, 467, 318, 125, 165, 809, 610, 31, 585, 970, 306, 42, 189, 169, 743, 78, 810, 70, 382, 367, 490, 787, 670, 476, 278, 775, 673, 299, 19, 893, 817, 971, 458, 409, 886, 434};
+//        int[] nums = {4,2,4,5,6};
         System.out.print(solution.maximumUniqueSubarray(nums));
     }
 
@@ -54,19 +55,43 @@ public class MaximumErasureValue {
             /**
              * set Or map
              */
-            int sum = 0, res = sum, start = 0;
-            Set<Integer> set = new HashSet<>();
+//            int sum = 0, res = sum, start = 0;
+//            Set<Integer> set = new HashSet<>();
+//            for (int i = 0; i < nums.length; i++) {
+//                int num = nums[i];
+//                if (!set.add(num)) {
+//                    while (nums[start] != nums[i]) {
+//                        set.remove(nums[start]);
+//                        sum -= nums[start];
+//                        start++;
+//                    }
+//                    sum -= nums[start];
+//                    start++;
+//                }
+//                sum += num;
+//                res = Math.max(res, sum);
+//            }
+//            return res;
+            /**
+             * 数组解法
+             */
+            int maxNumber = Integer.MIN_VALUE;
+            for (int num : nums) {
+                maxNumber = Math.max(maxNumber, num);
+            }
+            int[] temp = new int[maxNumber + 1];
+            int sum = 0, start = 0, res = sum;
             for (int i = 0; i < nums.length; i++) {
                 int num = nums[i];
-                if (!set.add(num)) {
-                    while (nums[start] != nums[i]) {
-                        set.remove(nums[start]);
-                        sum -= nums[start];
-                        start++;
+                int endIndex = temp[num];
+                if (endIndex != 0) {
+                    for (int j = start; j < endIndex; j++) {
+                        sum -= nums[j];
+                        temp[nums[j]] = 0;
                     }
-                    sum -= nums[start];
-                    start++;
+                    start = endIndex;
                 }
+                temp[num] = i + 1;
                 sum += num;
                 res = Math.max(res, sum);
             }
