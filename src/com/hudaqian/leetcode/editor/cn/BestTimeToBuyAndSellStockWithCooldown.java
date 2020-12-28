7b33beb707fb0e44aa5b2fc6df785149a56ca47c
hudaqian <458331943@qq.com>
2020-12-28 17:55:02 +0800

    309.最佳买卖股票时机含冷冻期
    dp解法 空间优化

--- src/com/hudaqian/leetcode/editor/cn/BestTimeToBuyAndSellStockWithCooldown.java
+++ src/com/hudaqian/leetcode/editor/cn/BestTimeToBuyAndSellStockWithCooldown.java
@@ -29,16 +29,30 @@ public class BestTimeToBuyAndSellStockWithCooldown {
             /**
              * dp解法
              */
+//            if (prices == null || prices.length == 0) return 0;
+//            int len = prices.length;
+//            int[][] dp = new int[len][2];
+//            dp[0][0] = 0;
+//            dp[0][1] = -prices[0];
+//            for (int i = 1; i < len; i++) {
+//                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+//                dp[i][1] = Math.max(dp[i - 1][1], (i >= 2 ? dp[i - 2][0] : 0) - prices[i]);
+//            }
+//            return dp[len - 1][0];
+            /**
+             * 空间优化
+             */
             if (prices == null || prices.length == 0) return 0;
             int len = prices.length;
-            int[][] dp = new int[len][2];
-            dp[0][0] = 0;
-            dp[0][1] = -prices[0];
+            int prevNoHaveProfit = 0, noHaveProfit = 0, haveProfit = - prices[0];
             for (int i = 1; i < len; i++) {
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-                dp[i][1] = Math.max(dp[i - 1][1], (i >= 2 ? dp[i - 2][0] : 0) - prices[i]);
+                int nextNoHaveProfit = Math.max(noHaveProfit, haveProfit+prices[i]);
+                int nextHaveProfit = Math.max(haveProfit,prevNoHaveProfit-prices[i]);
+                prevNoHaveProfit = noHaveProfit;
+                noHaveProfit = nextNoHaveProfit;
+                haveProfit = nextHaveProfit;
             }
-            return dp[len - 1][0];
+            return noHaveProfit;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
