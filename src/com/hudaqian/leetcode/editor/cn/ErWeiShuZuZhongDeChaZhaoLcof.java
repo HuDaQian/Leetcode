834761e05fcf71ee81f116fbe26e3eaf84974145
hudaqian <458331943@qq.com>
2021-01-04 18:19:55 +0800

    剑指 Offer 04. 二维数组中的查找
    矩阵分割+递归

--- src/com/hudaqian/leetcode/editor/cn/ErWeiShuZuZhongDeChaZhaoLcof.java
+++ src/com/hudaqian/leetcode/editor/cn/ErWeiShuZuZhongDeChaZhaoLcof.java
@@ -15,7 +15,7 @@
 //  [10, 13, 14, 17, 24],
 //  [18, 21, 23, 26, 30]
 //]
-// 
+//
 //
 // 给定 target = 5，返回 true。 
 //
@@ -38,10 +38,14 @@
 
 package com.hudaqian.leetcode.editor.cn;
 
+import java.util.Arrays;
+
 public class ErWeiShuZuZhongDeChaZhaoLcof {
     public static void main(String[] args) {
         Solution solution = new ErWeiShuZuZhongDeChaZhaoLcof().new Solution();
-
+        int[][] matrix = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+        int target = 5;
+        System.out.print(solution.findNumberIn2DArray(matrix, target));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -50,8 +54,8 @@ public class ErWeiShuZuZhongDeChaZhaoLcof {
             /**
              * 遍历解法
              */
-//            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
-//            int row = 0, col = matrix[0].length - 1;
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+            int row = 0, col = matrix[0].length - 1;
 //            int m = matrix.length;
 //            while (row < m && col >= 0) {
 //                int num = matrix[row][col];
@@ -63,60 +67,116 @@ public class ErWeiShuZuZhongDeChaZhaoLcof {
 //                    col--;
 //                }
 //            }
-            return false;
+//            return false;
             /**
              *  分割矩阵解法
              *  从中间将矩阵分割成四个部分 每个矩阵最小值就是左上顶点 最大值就是右下顶点 来判断target是否在矩阵内
              *  不断分割 最终结果是矩阵空间为0 或者空间为1 找到target
              */
-
+            int[] points = new int[4];
+            points[0] = 0;
+            points[1] = 0;
+            points[2] = matrix.length-1;
+            points[3] = matrix[0].length-1;
+            return findNumberIn2DArray(matrix, target, points);
+        }
+        private boolean findNumberIn2DArray(int[][] matrix, int target, int[] points) {
+            boolean res = false;
+            int[][] temp = getSubMatrix(matrix, points);
+            for (int i = 0; i < 4; i++) {
+                int[] checked = temp[i];
+                int checkNum = checkMatrixSubTarget(matrix, target, checked);
+                if (checkNum == 2) {
+                    return true;
+                }
+                if (checkNum == 1) {
+                    res = findNumberIn2DArray(matrix, target, checked) || res;
+                }
+            }
+            return res;
         }
 
-        private int[][] getSubMatrix(int[][] matrix) {
+
+        private int checkMatrixSubTarget(int[][] matrix, int target, int[] points) {
+            /**
+             * -1 oneBoxNoHaveNumber
+             * 0 noNumber
+             * 1 haveNumber
+             * 2 oneBoxHaveNumber
+             */
+            for (int i = 0; i < 4; i++) {
+                if (points[i] == -1) {
+                    return -1;
+                }
+            }
+            int min = matrix[points[0]][points[1]];
+            int max = matrix[points[2]][points[3]];
+            if (points[0] == points[2] && points[1] == points[3]) {
+                if (target == min) {
+                    return 2;
+                } else {
+                    return -1;
+                }
+            } else {
+                if (target >= min && target <= max) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        private int[][] getSubMatrix(int[][] matrix, int[] points) {
             //  四个矩阵 四个数字分别代表其左上顶点和右下顶点的坐标
             int[][] res = new int[4][4];
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    res[i][j] = -1;
+                }
+            }
             if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return res;
-            int m = matrix.length, n = matrix[0].length;
+            int sx = points[0], sy = points[1], ex = points[2], ey = points[3];
+            int m = ex-sx, n = ey-sy;
             //  这里把不符合条件的矩阵四角都设定成了-1 实际上不用这么麻烦 有一个-1就表示矩阵无效了
             //  左上矩阵
             int aX = 0, aY = 0;
             int bX = m/2, bY = n/2;
             //  右下矩阵
-            int cX = bX+1 >= m ? -1 : bX+1;
-            int cY = bY+1 >= n ? -1 : bY+1;
-            int dX = cX == -1 ? -1 : m-1;
-            int dY = cY == -1 ? -1 : n-1;
+            int cX = bX+1 > m ? -1 : bX+1;
+            int cY = bY+1 > n ? -1 : bY+1;
+            int dX = cX == -1 ? -1 : m;
+            int dY = cY == -1 ? -1 : n;
             //  左下矩阵
             int eX = cX;
             int eY = eX == -1 ? -1: 0;
-            int fX = eX == -1 ? -1 : m-1;
+            int fX = eX == -1 ? -1 : m;
             int fY = fX == -1 ? -1 : bY;
             //  右上矩阵
             int gY = cY;
             int gX = gY == -1 ? -1 : 0;
             int hY = dY;
-            int hX = hY == -1 ? -1 : 0;
+            int hX = hY == -1 ? -1 : bX;
 
             int[] leftTop = new int[4];
-            leftTop[0] = aX;
-            leftTop[1] = aY;
-            leftTop[2] = bX;
-            leftTop[3] = bY;
+            leftTop[0] = aX+sx;
+            leftTop[1] = aY+sy;
+            leftTop[2] = bX+sx;
+            leftTop[3] = bY+sy;
             int[] rightBottom = new int[4];
-            rightBottom[0] = cX;
-            rightBottom[1] = cY;
-            rightBottom[2] = dX;
-            rightBottom[3] = dY;
+            rightBottom[0] = cX+sx;
+            rightBottom[1] = cY+sy;
+            rightBottom[2] = dX+sx;
+            rightBottom[3] = dY+sy;
             int[] leftBottom = new int[4];
-            leftBottom[0] = eX;
-            leftBottom[1] = eY;
-            leftBottom[2] = fX;
-            leftBottom[3] = fY;
+            leftBottom[0] = eX+sx;
+            leftBottom[1] = eY+sy;
+            leftBottom[2] = fX+sx;
+            leftBottom[3] = fY+sy;
             int[] rightTop = new int[4];
-            rightTop[0] = gX;
-            rightTop[1] = gY;
-            rightTop[2] = hX;
-            rightTop[3] = hY;
+            rightTop[0] = gX+sx;
+            rightTop[1] = gY+sy;
+            rightTop[2] = hX+sx;
+            rightTop[3] = hY+sy;
 
             res[0] = leftTop;
             res[1] = rightBottom;
