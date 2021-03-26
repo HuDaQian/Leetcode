6cca119980474729e0bad3fdcd1138442d2abb85
hudaqian <458331943@qq.com>
2021-03-26 09:11:33 +0800

    83. 删除排序链表中的重复元素

--- src/com/hudaqian/leetcode/editor/cn/RemoveDuplicatesFromSortedList.java
+++ src/com/hudaqian/leetcode/editor/cn/RemoveDuplicatesFromSortedList.java
@@ -57,7 +57,17 @@ public class RemoveDuplicatesFromSortedList {
      */
     class Solution {
         public ListNode deleteDuplicates(ListNode head) {
-            if (head == null || head.next == null) {
+//                if (head == null) return head;
+//                ListNode current = head;
+//                while (current.next != null) {
+//                    if (current.next.val == current.val) {
+//                        current.next = current.next.next;
+//                    } else {
+//                        current = current.next;
+//                    }
+//                }
+//                return head;
+            if (head == null) {
                 return head;
             }
             ListNode slow = head;
